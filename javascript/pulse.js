d62a747c57fd8e635b4d44701bfbf1b82c385dfb
Joseph Vanderzwart <josephvanderzwart@protonmail.com>
2026-06-16 13:58:29 -0600

    move pages to root

---
+++ javascript/pulse.js
@@ -0,0 +1,23 @@
+<!--
+
+function test (){
+	alert("hello world");
+};
+
+function less (){
+	document.getElementById("flashing").border= "1px solid black";
+}
+
+function more (){
+	document.getElementById("flashing").border= "3px solid black";
+}
+
+function flash (){
+	less;
+	setTimeout(more,50);
+}
+
+function flashy (){
+	setInterval(flash,50);
+}
+//-->
